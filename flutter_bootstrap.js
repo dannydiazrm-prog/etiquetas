3876edc8ddf04d3bc9bc2d4a12dd4b1ada80c8c6
dannydiazrm-prog <dannydiazrm@gmail.com>
2026-05-03 21:21:34 +0000

    Deploying to gh-pages from @ dannydiazrm-prog/etiquetas@269538a71ee1a0c90c5098160e78f5904ca54bf5 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4117384698" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3819234604" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
